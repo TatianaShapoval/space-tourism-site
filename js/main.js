6e0b05594f95ba676cd26aa22279d211e6ab129a
TatianaShapoval <questopunto@gmail.com>
2021-10-23 04:28:51 +0300

    added contacts and footer

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 $(function(){
-
+    $('.slider__box').slick({
+        prevArrow: '<img class="slider__arrow slider__arrow-left " src="./images/icons/arrow2.svg" alt="arrow left" >',
+        nextArrow: '<img class="slider__arrow slider__arrow-right" src="./images/icons/arrow1.svg" alt="arrow right" >',
+    });
 });
