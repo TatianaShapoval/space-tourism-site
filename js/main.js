bb40734190b9bad47b544c8ab1942bd0e1f92b02
TatianaShapoval <questopunto@gmail.com>
2021-10-24 04:27:59 +0300

    added header adaptive

--- js/main.js
+++ js/main.js
@@ -3,4 +3,7 @@ $(function(){
         prevArrow: '<img class="slider__arrow slider__arrow-left " src="./images/icons/arrow2.svg" alt="arrow left" >',
         nextArrow: '<img class="slider__arrow slider__arrow-right" src="./images/icons/arrow1.svg" alt="arrow right" >',
     });
+    $('.menu-btn').on('click', function(){
+        $('.menu__list').toggleClass('active');
+    });
 });
