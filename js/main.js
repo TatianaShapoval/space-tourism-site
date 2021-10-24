9dc5f6046c5bcb2c14f031089fedc4be93ce7f9b
TatianaShapoval <questopunto@gmail.com>
2021-10-25 00:21:46 +0300

    added adaptive

--- js/main.js
+++ js/main.js
@@ -2,6 +2,14 @@ $(function(){
     $('.slider__box').slick({
         prevArrow: '<img class="slider__arrow slider__arrow-left " src="./images/icons/arrow2.svg" alt="arrow left" >',
         nextArrow: '<img class="slider__arrow slider__arrow-right" src="./images/icons/arrow1.svg" alt="arrow right" >',
+        responsive: [
+            {
+                breakpoint: 481,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
     });
     $('.menu-btn').on('click', function(){
         $('.menu__list').toggleClass('active');
